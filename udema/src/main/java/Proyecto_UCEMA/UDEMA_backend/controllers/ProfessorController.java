1cd23d261b790ab5d286da0af66d9998c58b4823
LucioFex <luciano1d2esteban@gmail.com>
2024-07-26 00:25:04 -0300

    feat: Support to the local port '4200' for Frontend apps (prevents CORS error)

--- udema/src/main/java/Proyecto_UCEMA/UDEMA_backend/controllers/ProfessorController.java
+++ udema/src/main/java/Proyecto_UCEMA/UDEMA_backend/controllers/ProfessorController.java
@@ -1,13 +1,8 @@
 package Proyecto_UCEMA.UDEMA_backend.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import Proyecto_UCEMA.UDEMA_backend.models.Professor;
-import Proyecto_UCEMA.UDEMA_backend.services.ProfessorServiceImpl;
-import io.swagger.v3.oas.annotations.Operation;
-
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Proyecto_UCEMA.UDEMA_backend.models.Professor;
+import Proyecto_UCEMA.UDEMA_backend.services.ProfessorServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 
 
 @RestController
 @RequestMapping(path = "api/professors")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProfessorController {
 	private final ProfessorServiceImpl professorService;
 
--- udema/src/main/java/Proyecto_UCEMA/UDEMA_backend/controllers/StudentController.java
+++ udema/src/main/java/Proyecto_UCEMA/UDEMA_backend/controllers/StudentController.java
@@ -1,16 +1,25 @@
 package Proyecto_UCEMA.UDEMA_backend.controllers;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import Proyecto_UCEMA.UDEMA_backend.models.Student;
 import Proyecto_UCEMA.UDEMA_backend.services.StudentServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 
-import org.springframework.web.bind.annotation.PutMapping;
-
 
 @RestController
 @RequestMapping(path = "api/students")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 	private final StudentServiceImpl studentService;
 
